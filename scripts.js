963da1d40865b859dbe5ef9f92ecc49ac36e3167
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-11-04 21:00:01 +0100

    ajout des sons

--- scripts.js
+++ scripts.js
@@ -1,14 +1,56 @@
+/*-----  toggle bouton son (on/off)--------*/
+
+let soundOn = true;
+/* for (let btnSound of btnSoundAll) {
+    btnSound.addEventListener('click', () => {
+        if (soundOn === true) {
+            soundOn = false;
+            btnSound.textContent = 'son (off)';
+        } else {
+            soundOn = true;
+            btnSound.textContent = 'son (on)';
+        }
+        return;
+    });
+} */
+
+/*-----  JOUER UN SON AVEC REGLAGE VOLUME --------*/
+
+const playSound = (src, vol) => {
+  let sound = new Audio(src);
+  if (soundOn === true) {
+    sound.play();
+    sound.volume = vol;
+  } else {
+    sound.pause();
+  }
+};
+
+window.onload = function () {
+  playSound('./sound/intro.mp3', 0.5);
+};
+
+// TO DO LIST
+
+// aléatoire placement carte
+// début et fin de partie
+//bug click 2 fois la meme carte
+// bouton pour relancer une game
+// animation victoire
+
 // toutes les cartes
 let allCards = document.querySelectorAll('.item');
 
+// déclarer tableau de toutes les cartes
+let arrayCards = [];
 
+// effet a l intro
 allCards.forEach((card) => {
   card.classList.add('intro');
 });
 
-
 let cptClickCurrent = 0;
-let dataImageShowed;
+let cardClikedId;
 
 // pour chaque carte au click -> playgame(card)
 allCards.forEach((card) => {
@@ -31,29 +73,40 @@ function playGame(card) {
     });
     // montre image cliquée
     card.classList.remove('hidden');
-    // stock reponse (grace a l attribut data-image des div)
-    dataImageShowed = card.dataset.image;
+    // stock id de l'image
+    cardClikedId = card.id;
 
     // --------------------------------------- 2eme click
   } else if (cptClickCurrent === 2) {
-    // montre image cliquée
-    card.classList.remove('hidden');
-    // ================ gagné===================
-    if (card.dataset.image === dataImageShowed) {
-      // pour toutes les cartes
-      allCards.forEach((card) => {
-        // si trouvé
-        if (!card.classList.contains('hidden')) {
-          // ajoute classe finded
-          card.classList.add('finded');
-        }
-      });
+    // si la carte que j'ai clické = carte déja cliqué stop
+    if (cardClikedId === card.id) {
+      // j annule le click
+      cptClickCurrent = 1;
+      // sinon je continu
+    } else {
+      // montre image cliquée
+      card.classList.remove('hidden');
+      // recup la carte cliqué
+      let cardClickedBefore = document.getElementById(cardClikedId);
+      // ================ gagné===================
+      if (cardClickedBefore.dataset.image === card.dataset.image) {
+        playSound('./sound/foundCard.mp3', 0.3);
+        // pour toutes les cartes
+        allCards.forEach((card) => {
+          // si trouvé
+          if (!card.classList.contains('hidden')) {
+            // ajoute classe finded
+            card.classList.add('finded');
+          }
+        });
+      } else {
+        playSound('./sound/lose.mp3', 0.2);
+      }
+      cptClickCurrent = 0;
+      cardClikedId = '';
+
+      // compter les cartes pas trouvées
+      // si 0 alors gagné, jeu fini
     }
-    cptClickCurrent = 0;
-    dataImageShowed = '';
-
-// compter les cartes pas trouvées
-// si 0 alors gagné, jeu fini
-
   }
 }
