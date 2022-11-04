82948e59892dc6c6708dc41dc522dae39dcd2d27
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-11-04 15:10:58 +0100

    script de base du jeu

--- scripts.js
+++ scripts.js
@@ -1,13 +1,49 @@
 // toutes les cartes
 let allCards = document.querySelectorAll('.item');
-// pour chaque carte au click
+
+let cptClickCurrent = 0;
+let dataImageShowed;
+
+// pour chaque carte au click -> playgame(card)
 allCards.forEach((card) => {
   card.addEventListener('click', () => {
-    // si elle a la classe hidden je la supprime
-    if (card.classList.contains('hidden')) {
-      card.classList.remove('hidden');
-    } else { // sinon je l ajoute
-      card.classList.add('hidden');
-    } 
+    playGame(card);
   });
 });
+
+function playGame(card) {
+  cptClickCurrent++;
+
+  // -------------------------------------------- 1er click
+  if (cptClickCurrent === 1) {
+    // parcours les cartes
+    allCards.forEach((card) => {
+      // cache les images non trouvées
+      if (!card.classList.contains('finded')) {
+        card.classList.add('hidden');
+      }
+    });
+    // montre image cliquée
+    card.classList.remove('hidden');
+    // stock reponse (grace a l attribut data-image des div)
+    dataImageShowed = card.dataset.image;
+
+    // --------------------------------------- 2eme click
+  } else if (cptClickCurrent === 2) {
+    // montre image cliquée
+    card.classList.remove('hidden');
+    // ================ gagné===================
+    if (card.dataset.image === dataImageShowed) {
+      // pour toutes les cartes
+      allCards.forEach((card) => {
+        // si trouvé
+        if (!card.classList.contains('hidden')) {
+          // ajoute classe finded
+          card.classList.add('finded');
+        }
+      });
+    }
+    cptClickCurrent = 0;
+    dataImageShowed = '';
+  }
+}
